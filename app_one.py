5880f5de50ca04d4e9631e8d8c9385ba0cd972f4
janvidixit <117842449+janvidixit@users.noreply.github.com>
2023-02-17 19:52:21 +0530

    Add files via upload

---
+++ app_one.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.title("Demo App🙂")
+
+n1 = st.number_input('enter number')
+n2 = st.number_input('enter another number')
+c1, c2, c3, c4, c5, c6 = st.columns(6)
+add = c1.button("Add")
+sub = c2.button('Subtract')
+mul = c3.button("multiply")
+div = c4.button("Divide")
+expo = c5.button("exponent")
+mod = c6.button("Modulus")
+if add:
+    r = int(n1) + int(n2)
+    st.success(r)
+if sub:
+    r = int(n1) - int(n2)
+    st.success(r)
+if mul:
+    r = int(n1) * int(n2)
+    st.success(r)
+if div:
+    r = int(n1) / int(n2)
+    st.success(r)  
+if expo: 
+    r = int(n1) ** int(n2)
+    st.success(r)      
+if mod:
+    r = int(n1) // int(n2)
+    st.success(r)         
